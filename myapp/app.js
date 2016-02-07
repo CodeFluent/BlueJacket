9795837ae3cfc3c48be1048533c9a1ed31e3ef1d
CodeFluent <wasfimomenwim@gmail.com>
2016-02-07 01:53:18 -0500

    Fixed the main app

--- myapp/app.js
+++ myapp/app.js
@@ -14,12 +14,10 @@ app.use(express.static('public'));
 
 app.get('/instagram',function(req,res){
 
-
+     res.sendFile('main.html');
+     res.sendFile('main.css');
      res.sendFile('index.js');
-     res.sendFile('index.html');
-     res.sendFile('gallery.html');
-     res.sendFile('contact.html');
-
+     res.sendFile('index.html')
 
 });
 
